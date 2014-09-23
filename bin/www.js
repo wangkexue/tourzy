6cb856b3ed36fec704560f00b31831a407fd4839
Zhiyuan Wang <zywang@u.northwestern.edu>
2014-09-22 19:36:39 -0500

    change www to work with heroku

--- bin/www.js
+++ bin/www.js
@@ -2,8 +2,8 @@
 var debug = require('debug')('tourzy');
 var app = require('../app');
 
-//app.set('port', process.env.PORT || 18080);
-app.set('port', 18080);
+app.set('port', process.env.PORT || 5000);
+//app.set('port', 18080);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
